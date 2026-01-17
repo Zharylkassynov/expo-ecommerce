819a290313001556d2db8c290a4407fd8254b874
Zharylkassynov <artur.zharylkassynov@narxoz.kz>
2026-01-17 09:10:10 +0500

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+
+const __dirname = path.resolve()
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({message: "Success!"});
 });
 
-app.listen(3000, () => console.log("Server is up and running"))
+// make our app ready for deployment
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+    app.get("/{*any}",(req,res) => {
+        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+    });
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`))

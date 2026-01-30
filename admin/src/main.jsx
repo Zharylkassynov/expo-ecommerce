ce346eae7ce6c3ab1fa891530b6215894d109809
Zharylkassynov <artur.zharylkassynov@narxoz.kz>
2026-01-30 11:20:18 +0500

    customers page & sentry added

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as Sentry from "@sentry/react";
 
 import {
     QueryClient,
@@ -23,6 +24,20 @@ if (!PUBLISHABLE_KEY) {
 }
 
 const queryClient = new QueryClient()
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true,
+    enableLogs: true,
+
+    integrations: [Sentry.replayIntegration()],
+    // Session replay
+    replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development
+    // and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>

66b7a2c8876f48ddcebdeaf71ec43b84caaefdcd
Zharylkassynov <artur.zharylkassynov@narxoz.kz>
2026-02-16 08:36:46 +0500

    tiny fixes

--- mobile/app/product/[id].tsx
+++ mobile/app/product/[id].tsx
@@ -19,8 +19,8 @@ import {
 const {width} = Dimensions.get("window");
 
 function ProductDetailScreen() {
-    const {id} = useLocalSearchParams()
-    const {data: product, isError, isLoading} = useProduct(id as string);
+    const {id} = useLocalSearchParams<{id:string}>()
+    const {data: product, isError, isLoading} = useProduct(id);
     const {addToCart, isAddingToCart} = useCart();
     const {isInWishlist, toggleWishlist, isAddingToWishlist, isRemovingFromWishlist} = useWishlist();
     const [selectedImageIndex, setSelectedImageIndex] = useState(0);

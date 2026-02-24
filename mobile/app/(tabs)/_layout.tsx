e51c37cc940f3954eb9e1075d135332a297cfc85
Zharylkassynov <artur.zharylkassynov@narxoz.kz>
2026-02-24 11:05:13 +0500

    fix: tab navigation bar

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,26 +1,9 @@
-import { Redirect } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { useAuth } from "@clerk/clerk-expo";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { BlurView } from "expo-blur";
-import { Platform, StyleSheet } from "react-native";
-
-import { withLayoutContext } from "expo-router";
-import {
-    createNativeBottomTabNavigator,
-    NativeBottomTabNavigationOptions,
-    NativeBottomTabNavigationEventMap
-} from "@bottom-tabs/react-navigation";
-import { ParamListBase, TabNavigationState } from "@react-navigation/native";
-
-const BottomTabNavigator = createNativeBottomTabNavigator().Navigator;
-
-const Tabs = withLayoutContext<
-    NativeBottomTabNavigationOptions,
-    typeof BottomTabNavigator,
-    TabNavigationState<ParamListBase>,
-    NativeBottomTabNavigationEventMap
->(BottomTabNavigator);
+import { StyleSheet } from "react-native";
 
 const TabsLayout = () => {
     const { isSignedIn, isLoaded } = useAuth();
@@ -30,29 +13,59 @@ const TabsLayout = () => {
     if (!isSignedIn) return <Redirect href={"/(auth)"} />;
 
     return (
-       <Tabs>
-           <Tabs.Screen
-            name={"index"}
-            options={{
-                title: "Shop",
-                tabBarIcon: () => ({ sfSymbol: "house" })
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: "#1DB954",
+                tabBarInactiveTintColor: "#B3B3B3",
+                tabBarStyle: {
+                    position: "absolute",
+                    backgroundColor: "transparent",
+                    borderTopWidth: 0,
+                    height: 32 + insets.bottom,
+                    paddingTop: 4,
+                    marginHorizontal: 100,
+                    marginBottom: insets.bottom,
+                    borderRadius: 24,
+                    overflow: "hidden",
+                },
+                tabBarBackground: () => (
+                    <BlurView
+                        intensity={80}
+                        tint="dark"
+                        style={StyleSheet.absoluteFill}
+                        // StyleSheet.absoluteFill is equal to this 👇
+                        // { position: "absolute", top: 0, right: 0, left: 0, bottom: 0 }
+                    />
+                ),
+                tabBarLabelStyle: {
+                    fontSize: 12,
+                    fontWeight: 600,
+                },
+                headerShown: false,
             }}
-           />
-           <Tabs.Screen
-               name={"cart"}
-               options={{
-                   title: "Cart",
-                   tabBarIcon: () => ({ sfSymbol: "person" })
-               }}
-           />
-           <Tabs.Screen
-               name={"profile"}
-               options={{
-                   title: "Profile",
-                   tabBarIcon: () => ({ sfSymbol: "person" })
-               }}
-           />
-       </Tabs>
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Shop",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="grid" size={size} color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="cart"
+                options={{
+                    title: "Cart",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="cart" size={size} color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({ color, size }) => <Ionicons name="person" size={size} color={color} />,
+                }}
+            />
+        </Tabs>
     );
 };
 
